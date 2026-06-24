32fc76d964714a936a09c272ce267f763d64bce3
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-06-24 12:03:24 +0530

    Module 1: Spring Core Basics and Constructor Injection

---
+++ Module-01-Spring-Core/learningSpringboot/src/main/java/com/amrita/learningspringboot/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.amrita.learningspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/")
+    public String hello(){
+        return "Hello Amrita!";
+    }
+}
