0a3cb4dda3f22a12e67df93b92feb041d8c1e6ce
Amrita Swain <swainamrita2004@gmail.com>
2026-08-31 23:31:24 +0530

    updated to new

--- Module-02-Spring-Boot/mapping/src/main/java/com/example/mapping/DepartmentRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.mapping;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  DepartmentRepo extends JpaRepository<Department, Integer> {
-}
--- Module-02-Spring-Boot/mapping/src/main/java/com/example/mapping/EmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.mapping;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-
-}
