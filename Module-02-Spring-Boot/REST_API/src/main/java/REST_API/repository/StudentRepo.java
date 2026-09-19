0d0aa33092e1e63ebfc6d589eb3fb6c424a31d5f
Amrita Swain <swainamrita2004@gmail.com>
2026-09-19 11:03:12 +0530

    Delete Module-02-Spring-Boot/REST_API directory

--- Module-02-Spring-Boot/REST_API/src/main/java/REST_API/repository/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package REST_API.repository;
-
-import REST_API.entity.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepo extends CrudRepository<Student, Integer> {
-}
